2b9135118815bc8b9ece0b0d588c52f7b3548810
Md Awsaf Alam <1505114.maaa@ugrad.cse.buet.ac.bd>
2022-05-10 20:41:40 +0600

    changed server url

--- crawler/api/index.py
+++ crawler/api/index.py
@@ -1,7 +1,7 @@
 import requests
 
-# baseUrl = 'http://localhost:1111'
-baseUrl = 'http://server:1111'
+baseUrl = 'http://localhost:1111'
+# baseUrl = 'http://server:1111'
 
 def get(url, params = {}):
   try:
